15582413d4557dda9e7786811a06f586edd6abd1
Kayala Achyuth <103745084+Achyuth2004@users.noreply.github.com>
2025-01-16 15:06:04 +0530

    Update app.py

--- AgriSens-master/FERTILIZER-RECOMMENDATION/app.py
+++ AgriSens-master/FERTILIZER-RECOMMENDATION/app.py
@@ -6,7 +6,11 @@ from sklearn.preprocessing import LabelEncoder
 
 def load_models():
     """Load the trained models and encoders."""
-    classifier = pickle.load(open('classifier.pkl', 'rb'))
+    # classifier = pickle.load(open('classifier.pkl', 'rb'))
+    import os
+    with open(os.path.join(os.path.dirname(__file__), 'classifier.pkl'), 'rb') as file:
+    classifier = pickle.load(file)
+
     encoder = pickle.load(open('fertilizer.pkl', 'rb'))
     return classifier, encoder
 
