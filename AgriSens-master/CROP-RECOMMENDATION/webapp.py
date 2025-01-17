a28f836f0b8ddc6bccbb730903e1d64b33ffc498
Achyuth2004 <kk9015@srmist.edu.in>
2025-01-17 11:57:42 +0530

    edited

--- AgriSens-master/CROP-RECOMMENDATION/webapp.py
+++ AgriSens-master/CROP-RECOMMENDATION/webapp.py
@@ -18,11 +18,11 @@ from sklearn.preprocessing import LabelEncoder
 
 # Display Images
 # import Image from pillow to open images
-# from PIL import Image
-# img = Image.open("CROP-RECOMMENDATION\crop.png")
+from PIL import Image
+img = Image.open("CROP-RECOMMENDATION\crop.png")
 # display image using streamlit
 # width is used to set the width of an image
-st.image("crop.png",caption=" ")
+st.image(img)
 
 df= pd.read_csv('Crop_recommendation.csv')
 
