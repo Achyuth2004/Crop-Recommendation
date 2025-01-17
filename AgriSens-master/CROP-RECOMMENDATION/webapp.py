d4a00a3bcdf1107c9b8ef264c66169d3c1874ce1
Kayala Achyuth <103745084+Achyuth2004@users.noreply.github.com>
2025-01-17 12:09:43 +0530

    Update webapp.py

--- AgriSens-master/CROP-RECOMMENDATION/webapp.py
+++ AgriSens-master/CROP-RECOMMENDATION/webapp.py
@@ -18,25 +18,11 @@ from sklearn.preprocessing import LabelEncoder
 
 # Display Images
 # import Image from pillow to open images
-# from PIL import Image
-# img = Image.open("crop.png")
-# # display image using streamlit
-# # width is used to set the width of an image
-# st.image(img)
-
-import os
 from PIL import Image
-
-# Get the directory of the current script
-script_dir = os.path.dirname(os.path.abspath(Crop-Recommendation\AgriSens-master\CROP-RECOMMENDATION))
-
-# Construct the full path to the image
-image_path = os.path.join(script_dir, "crop.png")
-
-# Load the image
-img = Image.open(image_path)
+img = Image.open("crop.png")
+# display image using streamlit
+# width is used to set the width of an image
 st.image(img)
-
 
 df= pd.read_csv('Crop_recommendation.csv')
 
